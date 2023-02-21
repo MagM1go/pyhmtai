5ac358440503dd765b25a46b4bd3d24b7f72d983
MagM1go <magmigo2@inbox.ru>
2023-02-21 16:37:55 +0800

    add init

---
+++ pyhmtai/__init__.py
@@ -0,0 +1,1 @@
+from hmtai import *
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ README = (HERE / "README.md").read_text(encoding="utf8")
 
 setup(
 	name="pyhmtai",
-	version="0.1.2",
+	version="0.1",
 	author="MagMigo",
 	description="HMtai python port.",
 	long_description=README,
